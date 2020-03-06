7db9be2e6d5ea7f440ec10b0a759721a5b0fc1a5
madBeavis <57059024+madBeavis@users.noreply.github.com>
2020-03-05 22:02:53 -0700

    Add files via upload

--- numato/numato.py
+++ numato/numato.py
@@ -13,6 +13,7 @@ class Separator(Enum):
     Comma = 1
     Space = 2
 
+
 ###################################
 # functions
 ###################################
@@ -36,9 +37,8 @@ def connectToDevice(DeviceIPAddress):
     elif "denied" in log_result:
         return False
 
+
 # power off a list of devices
-
-
 def powerOffDevices(devices, delay, wait, silent=False):
     i = 1
     count_devices = len(devices)
@@ -62,6 +62,7 @@ def powerOffDevices(devices, delay, wait, silent=False):
     if not silent:
         print('\n')  # new line for progress bar
     return True, devices
+
 
 # power on a list of devices
 
@@ -90,12 +91,13 @@ def powerOnDevices(devices, delay, wait, silent=False):
         print('\n')  # new line for progress bar
     return True, devices
 
+
 # power cycle a list of devices
 
 
 def powerCycleDevices(devices, delay, wait, silent=False):
     if not silent:
-        print("Powering cycling devices " + devices )
+        print("Powering cycling devices " + devices)
 
     i = 1
     count_devices = len(devices)
@@ -119,7 +121,8 @@ def powerCycleDevices(devices, delay, wait, silent=False):
 
     if not silent:
         print('\n')  # new line for progress bar
-    return True,devices
+    return True, devices
+
 
 # reset the relay module
 
@@ -127,6 +130,7 @@ def powerCycleDevices(devices, delay, wait, silent=False):
 def resetRelayModule():
     telnet_obj.write(b"reset\r\n")
     return "\nReset successfully\n"
+
 
 def getRelayStatusAll():
     """Retrieve and print whole relay status"""
@@ -143,46 +147,31 @@ def getRelayStatusAll():
 
     return power_values
 
-def getRelayStatus(devices, delay, wait, silent=False):
-    # possible return values:
-    #   b'on\r\n'
-    #   b'off\n\n'
-    retValues = ""
-    i = 1
-    count_devices = len(devices)
 
-    # Flush any data that may be left in the input buffer
-    telnet_obj.read_eager()
+def getRelayStatus(devices, delay):
 
-    if count_devices > 1:
-        if not silent:
-            print('Collecting status for ' + str(count_devices) + ' devices, please wait.\n')
+    # Send Command
+    telnet_obj.write(b"relay readall\r\n")
+    time.sleep(delay)
 
-    for device_status in devices:
-        # Send command
-        telnet_obj.write(b"relay read " + device_status.encode("ascii") + b"\r\n")
-        time.sleep(wait)
+    # Read Relay Status
+    response = telnet_obj.read_until(b">")
+    power_values_hex = response.decode()[:-2]
 
-        # Read and process response from the device
-        response = telnet_obj.read_eager()
+    power_values = bin(int(power_values_hex, 16))[2:].zfill(32)[::-1]
+    return_power_values = ''
 
-        retValueBytes = re.split(br'[&>]', response)[0]
-        retValue = str(retValueBytes, 'ascii')
+    for this_device_status in devices:
+        ordinal = ord(this_device_status)
+        if ordinal <= 57:
+            index = ordinal - 48
+        else:
+            index = ordinal - 65 + 10
 
-        if 'on' in retValue:
-            retValues += '1'
-        elif 'off' in retValue:
-            retValues += '0'
+        return_power_values = return_power_values + power_values[index]
 
-        if not silent and count_devices > 1:
-            progressBar(i, count_devices, 32)
-            i += 1;
+    return return_power_values
 
-        time.sleep(delay)
-
-    if not silent:
-        print('\n')  # newline for progress bar
-    return retValues, devices
 
 def fixUpDeviceNames(devices):
     device_list = ""
@@ -192,7 +181,7 @@ def fixUpDeviceNames(devices):
 
     # check for flag for all devices
     if 'Z' in devices.upper():
-        return "0123456789ABCDEFGHIJKLMNOPQRSTUV", False
+        return "0123456789ABCDEFGHIJKLMNOPQRSTUV", Separator.Plain
 
     # check different types of ways we seperate
     if ',' in devices:  # check if comma separated for relays
@@ -200,7 +189,7 @@ def fixUpDeviceNames(devices):
 
         for this_device in list_of_devices:
             if this_device == "":
-                print ("Empty device specified, so it will be ignored\n")
+                print("Empty device specified, so it will be ignored\n")
             elif int(this_device) < 0:
                 print("Device below zero specified, so it will be ignored\n")
             elif int(this_device) < 32:
@@ -218,7 +207,7 @@ def fixUpDeviceNames(devices):
 
         for this_device in list_of_devices:
             if this_device == "":
-                print ("Empty device specified, so it will be ignored\n")
+                print("Empty device specified, so it will be ignored\n")
             elif int(this_device) < 0:
                 print("Device below zero specified, so it will be ignored\n")
             elif int(this_device) < 32:
@@ -242,8 +231,9 @@ def fixUpDeviceNames(devices):
 
     return device_list, Separator.Plain;
 
+
 def fixUpPowerOptions(power_options):
-    option_list=""
+    option_list = ""
 
     # remove quotation marks in case user enclosed
     power_options = power_options.replace('"', '').replace("'", '')
@@ -265,9 +255,10 @@ def fixUpPowerOptions(power_options):
     else:
         return power_options, Separator.Plain
 
+
 def progressBar(value, endvalue, bar_length=32):
     percent = float(value) / endvalue
-    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    arrow = '-' * int(round(percent * bar_length) - 1) + '>'
     spaces = ' ' * (bar_length - len(arrow))
 
     sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
@@ -283,24 +274,19 @@ def progressBar(value, endvalue, bar_length=32):
 if sys.version_info[0] < 3:
     raise Exception("Python version 3.x required")
 
-# Setup device and login info
-DeviceIP = "192.168.8.5"  # Device IP address
-user = "admin"  # Device Telnet user name
-password = "admin"  # Device Telnet password
-
 # Setup arguments
 parser = ArgumentParser()
 
 group1 = parser.add_mutually_exclusive_group()
 group1.add_argument("-ak", "--all_kill", action="store_true", dest='kill_all',
                     help="Turn off all devices")
-group1.add_argument("-ao", "--all_on",  action="store_true", dest='all_on',
+group1.add_argument("-ao", "--all_on", action="store_true", dest='all_on',
                     help="Turn on all devices")
 group1.add_argument("-pca", "--all_power_cycle", action="store_true", dest='power_cycle_all',
                     help="Power cycle devices all devices, i.e. off then on")
-group1.add_argument("-pc", "--power_cycle",  action="store_true", dest='power_cycle',
+group1.add_argument("-pc", "--power_cycle", action="store_true", dest='power_cycle',
                     help="Power cycle devices, i.e. off then on")
-group1.add_argument("-r", "--reset",  action="store_true", dest='reset',
+group1.add_argument("-r", "--reset", action="store_true", dest='reset',
                     help="Reset the relay module")
 group1.add_argument("-s", "--status", dest="status", action="store_true",
                     help="Query the status of devices")
@@ -319,8 +305,17 @@ parser.add_argument("-ip", "--ip_address", type=str, dest='ip_address',
 parser.add_argument("-off", "--off_first", dest='off_first',
                     help="Power off all devices before turning on devices")
 parser.add_argument('-si', '--silent', dest='silent', help='Suppress feedback from program', action='store_true')
+parser.add_argument('-ti', '--timeout', dest='timeout', help='Timeout for telnet connection, default=5', type=int, default=5)
+parser.add_argument('-port', '--port', dest='port', help='Port to use for telnet connection, default=23', type=int, default=23)
+parser.add_argument('-user', '--username', dest='username', help='Username to log into board with, default=admin', type=str, default ='admin')
+parser.add_argument('-pwd', '--password', dest='password', help='Password to log into board with, default=admin', type=str, default='admin')
 
 args = parser.parse_args()
+
+# Setup device and login info
+DeviceIP = args.ip_address  # Device IP address
+user = args.username  # Device Telnet user name
+password = args.password # Device Telnet password
 
 # checks for options to run
 if args.devices is None and args.status is not None:
@@ -359,8 +354,7 @@ if args.devices is not None and args.power_options is not None:
         exit(0)
 
 # Create a new TELNET object
-telnet_obj = telnetlib.Telnet(DeviceIP)
-
+telnet_obj = telnetlib.Telnet(host=DeviceIP, timeout=args.timeout, port=args.port)
 
 # Connect to the device using credentials provided
 if connectToDevice(DeviceIP):
@@ -382,7 +376,7 @@ if connectToDevice(DeviceIP):
         powerOnDevices(devices=devices_on, delay=args.delay, wait=args.wait, silent=args.silent)
 
     elif args.power_cycle_all:  # power cycle all devices that are currently on
-        devices_to_power_cycle = getRelayStatusAll()
+        devices_to_power_cycle = getRelayStatus(fixUpDeviceNames('Z')[0])
 
         for device_to_power_cycle in devices_to_power_cycle:
             if device_to_power_cycle == '1':
@@ -423,20 +417,25 @@ if connectToDevice(DeviceIP):
             i += 1
 
     elif args.status and args.devices is None:
-        returnValue = getRelayStatusAll()
-        print(returnValue)
+        devices = fixUpDeviceNames('Z')[0]
+        returnValue = getRelayStatus(devices, delay=args.delay)
+
+        print("Power Status for Device(s)")
+        print('Devices: ' + devices)
+        print('Status:  ' + returnValue)
+        print("")
+
         exit(returnValue)
 
-
     elif args.status and my_devices != "":
-        returnValue = getRelayStatus(my_devices, args.delay, args.wait, silent=args.silent)
+        returnValue = getRelayStatus(my_devices, delay=args.delay)
 
         print("Power Status for Device(s)")
         print("Devices:  " + my_devices)
-        print("Status:   " + returnValue[0])
+        print("Status:   " + returnValue)
         print("")
 
-        exit(returnValue[0])
+        exit(returnValue)
 else:
     print("Login failed!!!! Please check login credentials or Device IP Address\n")
     telnet_obj.close()
