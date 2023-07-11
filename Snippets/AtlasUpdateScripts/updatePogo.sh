3ed28d973430cf309d53d8faeecce3a63400c497
madBeavis <57059024+madBeavis@users.noreply.github.com>
2023-07-11 14:38:08 -0600

    Add files via upload

---
+++ Snippets/AtlasUpdateScripts/updatePogo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+adb disconnect
+for i in $(seq 101 199); do
+	adb connect 192.168.8.$i;
+done
+
+for j in $(seq 210 230); do
+	adb connect 192.168.8.$j;
+done
+
+readarray -d ' ' devices < <(adb devices -l | cut -f1 -d' ' | tail -n+2 | xargs);
+
+for device in "${devices[@]}"; do
+	echo "updating pogo on "$device;	
+	adb -s $device install -r "pogo.apk";
+done
